8046cf317e6567f278eb47c1a4a5907c4f706d1e
David Rodrigues <davidrod.contato@gmail.com>
2024-01-28 00:26:44 -0300

    adding the battle music using fmod.

---
+++ Assets/_Main/Scripts/FMODMusic.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    private static FMOD.Studio.EventInstance _music;
+    void Start()
+    {
+        _music = FMODUnity.RuntimeManager.CreateInstance("event:/BattleTheme");
+        _music.start();
+        _music.setVolume(0.4f);
+        _music.release();
+    }
+
+    private void OnDestroy()
+    {
+        _music.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+}
