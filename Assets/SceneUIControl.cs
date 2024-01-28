a6ddacd72cee0b55f305d0f71a441a88236a63b7
Russo <aleksander@ice.ufjf.br>
2024-01-28 04:52:17 -0300

    UI and scene control added

---
+++ Assets/SceneUIControl.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
+
+public class SceneUIControl : MonoBehaviour
+{
+	private int l;
+	public GameObject langButton;
+	public Sprite br;
+	public Sprite us;
+
+	public void Start()
+	{
+		l = PlayerPrefs.GetInt("language", 0);
+		langButton.GetComponent<Image>().sprite = l > 0 ? us : br;
+	}
+
+	public void PvP()
+	{
+		SceneManager.LoadScene("GambiarraPvP");
+	}
+
+	public void PvE()
+	{
+		SceneManager.LoadScene("GambiarraPvE");
+	}
+
+	public void Rematch()
+	{
+		SceneManager.LoadScene(GameObject.Find("Gambiarra").transform.position.x > 0 ? "GambiarraPvE" : "GambiarraPvP");
+	}
+
+	public void Quit()
+	{
+		SceneManager.LoadScene("TitleScene");
+	}
+
+	public void exitGame()
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#endif
+		Application.Quit();
+	}
+
+	public void toggleLanguage()
+	{
+		l = l > 0 ? 0 : 1;
+		PlayerPrefs.SetInt("language", l);
+		PlayerPrefs.Save();
+		GameObject.Find("EventSystem").GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(null);
+		langButton.GetComponent<Image>().sprite = l > 0 ? us : br;
+	}
+}
